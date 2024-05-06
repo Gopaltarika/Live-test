f2c2977f32614bb82a02abed0b8260f9e9680808
GopalAror <punjabijanu586@gmail.com>
2024-05-06 20:48:06 +0530

    hovereffect

--- src/components/HeaderCard.jsx
+++ src/components/HeaderCard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HeaderCard = (props) => {
     return (
-        <div className="col-lg-3 col-sm-6 mt-lg-0 mt-3">
-            <div className="card-body bg-card-gradient w-100 d-flex flex-column align-items-center justify-content-center gap-32" data-aos="flip-left">
+        <div className="col-lg-3 col-sm-6 mt-lg-0 mt-3" data-aos="flip-left">
+            <div className="card-body bg-card-gradient w-100 d-flex flex-column align-items-center justify-content-center gap-32">
                 <img src={props.Image} alt="images" />
                 <h5 dangerouslySetInnerHTML={{__html:props.heading}} className='m-0 ff-OpenSans fw-bold fs-md lh-md text-center text-white'></h5>
             </div>
--- src/components/SevenAiCard.jsx
+++ src/components/SevenAiCard.jsx
@@ -6,7 +6,7 @@ const SevenAiCard = (props) => {
             <div className={`card-sevenAi w-100 bg-card-gradient d-flex justify-content-center align-items-center ${props.maxWidth}`}>
                 {props.Images && <img src={props.Images} alt="Logo-Images" width={props.size} height={props.height} />}
               <div className='d-flex flex-column gap-14'>
-                  {props.heading &&  <h5 className='text-white ff-OpenSans fw-bold fs-lg lh-2lg m-0'>{props.heading}</h5>}
+                  {props.heading &&  <h5 className='text-white heading ff-OpenSans fw-bold fs-lg lh-2lg m-0'>{props.heading}</h5>}
                   {props.para &&  <p className='m-0 text-white opacity-80 ff-OpenSans fw-normal fs-sm'>{props.para}</p>}
               </div>
             </div>
